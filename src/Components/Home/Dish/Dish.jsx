1e97b021b773ea8d2a7875a0ade59b88876b5538
RuinedRipper <rostik.ushakov24@gmail.com>
2023-09-22 17:31:44 +0300

    Dish component 2.3

--- src/Components/Home/Dish/Dish.jsx
+++ src/Components/Home/Dish/Dish.jsx
@@ -1,25 +1,26 @@
 import styles from "./Dish.module.css";
 import RatingIMG from "../../../Images/RatingIMG.png";
+import { Link } from "react-router-dom";
 
 const Dish = (props) => {
   return (
-    <section className={styles.DishContain}>
-      <img src={props.image} />
+    <Link className={styles.DishContain} to={props.to}>
+      <img src={props.image} alt="Restaraunt" />
       <section className={styles.DishDescription}>
         <section className={styles.DishNameSection}>
           <h2 className={styles.DishName}>{props.DishName}</h2>
           <p className={styles.DishTime}>50 хв</p>
         </section>
         <section className={styles.DishRatingAndPrice}>
-          <img src={RatingIMG} />
+          <img src={RatingIMG} alt="Rating" />
           <p className={styles.Rating}>4.99</p>
-          <p className={styles.Price}>От 10 $</p>
+          <p className={styles.Price}>Від 5 $</p>
           <ul>
             <li>Піцца</li>
           </ul>
         </section>
       </section>
-    </section>
+    </Link>
   );
 };
 
