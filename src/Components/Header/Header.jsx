291a1c9039fbe39079c8b1aa9ffb68a839e6a354
RuinedRipper <rostik.ushakov24@gmail.com>
2023-10-11 19:29:10 +0300

    Added 4.1 Login modal

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import styles from "./Header.module.css";
+import AuthModal from "../AuthModal/AuthModal";
 import Cart from "../Cart/Cart";
 import { NavLink } from "react-router-dom";
 import Logo from "../../Images/logo.png";
@@ -7,8 +8,16 @@ import User from "../../Images/user.png";
 import CartImage from "../../Images/cart.png";
 import { useState } from "react";
 
+import { useDispatch } from "react-redux";
+import { logout } from "../../Store/loginSlice";
+import { useSelector } from "react-redux";
+
 const Header = () => {
+  const UserName = useSelector((state) => state.login.UserName);
+  const [IsOpenAuth, setOpenAuth] = useState(false);
+  const [IsAuth, setIsAuth] = useState(false);
   const [isOpenCart, setOpenCart] = useState(false);
+  const dispatch = useDispatch();
 
   const openCart = () => {
     setOpenCart(true);
@@ -18,9 +27,22 @@ const Header = () => {
     setOpenCart(false);
   };
 
+  const openAuthModal = () => {
+    setOpenAuth(true);
+  };
+
+  const handleLogin = () => {
+    dispatch(logout());
+    setIsAuth(false);
+  };
+
+  const closeAuthModal = () => {
+    setOpenAuth(false);
+  };
+
   return (
     <section className={styles.HeaderContain}>
-      <NavLink className={styles.Logo} to="/">
+      <NavLink className={styles.Logo} to="/DeliveryFood">
         <img src={Logo} alt="Логотип" />
         <span>
           <p>Delivery</p>
@@ -31,13 +53,33 @@ const Header = () => {
         <img src={Home} alt="Домашня сторінка" />
         <input placeholder="Адреса доставки" />
       </section>
+
+      {IsAuth && <p className={styles.UserName}>{UserName}</p>}
       <section className={styles.HeaderButtons}>
-        <button className={styles.Login}>
-          <img src={User} alt="Користувач" />
-          Увійти
-        </button>
+        {!IsAuth && (
+          <button className={styles.Login} onClick={openAuthModal}>
+            <img src={User} alt="Користувач" />
+            Увійти
+          </button>
+        )}
+
+        {IsOpenAuth && !IsAuth && (
+          <AuthModal
+            isOpen={IsOpenAuth}
+            IsAuth={setIsAuth}
+            onCloseCart={closeAuthModal}
+          />
+        )}
+
+        {IsAuth && (
+          <button className={styles.Login} onClick={handleLogin}>
+            <img src={User} alt="Пользователь" />
+            Вийти
+          </button>
+        )}
+
         <button className={styles.Cart} onClick={openCart}>
-          <img src={CartImage} alt="Кошик" />
+          <img src={CartImage} alt="Корзина" />
           Кошик
         </button>
 
